6b70622e2a3b5bd12710092a41d9af3e39441406
Patrick Mota <patrickvieiramota@gmail.com>
2021-07-09 14:49:56 -0300

    Adicionar interface na conta

--- bytebank-herdado-conta/src/ContaCorrente.java
+++ bytebank-herdado-conta/src/ContaCorrente.java
@@ -1,4 +1,4 @@
-public class ContaCorrente extends Conta{
+public class ContaCorrente extends Conta implements Tributavel{
 
     public ContaCorrente(int agencia, int numero){//tenho que criar esse construtor pois a classe mãe tem um
         super(agencia, numero);// chamando o construtor da classe mãe
@@ -13,5 +13,10 @@ public class ContaCorrente extends Conta{
     public boolean saca(double valor) {
         double valorASacar = valor + 0.2;
         return super.saca(valorASacar);
+    }
+
+    @Override
+    public double getValorImposto() {
+        return super.getSaldo() + 0.01;
     }
 }
