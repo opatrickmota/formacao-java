7ad27cf7a621a496391c7d28fcaab9f0cd4f8798
Patrick Mota <patrickvieiramota@gmail.com>
2021-07-09 14:10:09 -0300

    Composicao com interface

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,6 +1,10 @@
 public class Gerente extends Funcionario implements Autenticavel{
 
-    private int senha;
+    private AutenticacaoUtil autenticador;
+
+    public Gerente(){
+        this.autenticador = new AutenticacaoUtil();
+    }
 
     //novo método, recebendo dois params. Isso é sobrecarga
     public boolean autentica(String login, int senha) {
@@ -14,14 +18,11 @@ public class Gerente extends Funcionario implements Autenticavel{
 
     @Override
     public void setSenha(int senha) {
-        this.senha = senha;
+        this.autenticador.setSenha(senha);
     }
 
     @Override
     public boolean autentica(int senha){
-        if(this.senha == senha){
-            return true;
-        }
-        return false;
+        return this.autenticador.autentica(senha);
     }
 }
