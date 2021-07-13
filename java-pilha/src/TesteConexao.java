ed38f423edb1327998736300cca04e53791be105
Patrick Mota <patrickvieiramota@gmail.com>
2021-07-13 18:19:56 -0300

    Aplicar try with resources

--- java-pilha/src/TesteConexao.java
+++ java-pilha/src/TesteConexao.java
@@ -1,13 +1,20 @@
 public class TesteConexao {
     public static void main(String[] args) {
-        Conexao con = null;
+
+        try(Conexao conexao = new Conexao()){
+            conexao.leDados();
+        }catch(IllegalStateException ex){
+            System.out.println("Erro na conexao");
+        }
+
+        /*Conexao con = null;
         try{
             con = new Conexao();
             con.leDados();
         }catch (IllegalStateException ex){
             System.out.println("Deu erro na conexão");
         }finally {
-            con.fecha();
-        }
+            con.close();
+        }*/
     }
 }
