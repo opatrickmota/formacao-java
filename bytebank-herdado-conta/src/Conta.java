d370d2292e42f1284072bf9a2ef1fa71f6d3ef12
Patrick Mota <patrickvieiramota@gmail.com>
2021-07-13 11:12:42 -0300

    Refatorar excecao para ser do tipo checked

--- bytebank-herdado-conta/src/Conta.java
+++ bytebank-herdado-conta/src/Conta.java
@@ -58,14 +58,14 @@ public abstract class Conta {
 
     public abstract void deposita(double valor);
 
-    public void saca(double valor){
+    public void saca(double valor) throws SaldoInsuficienteException{
         if(this.saldo < valor){
             throw new SaldoInsuficienteException("Saldo: "+this.saldo + ", Valor: "+valor);
         }
         this.saldo -= valor;
     }
 
-    public void transfere(double valor, Conta destino){
+    public void transfere(double valor, Conta destino) throws SaldoInsuficienteException{
         saca(valor);
         destino.deposita(valor);
     }
