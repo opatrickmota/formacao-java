2c7be7d5c13274054d22a6a123442b79940a1abf
Patrick Mota <patrickvieiramota@gmail.com>
2021-07-14 10:36:54 -0300

    Gerar documentacao: javadoc

--- bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -1,5 +1,10 @@
 package br.com.bytebank.banco.modelo;
 
+/**
+ * Classe representa a moldura de uma conta
+ * @author Patrick Mota
+ */
+
 public abstract class Conta {
     protected double saldo;
     private int agencia;
@@ -7,6 +12,11 @@ public abstract class Conta {
     private Cliente titular;
     private static int total; // esse atributo agora é da classe modelo.Conta e nao mais dos seus objetos
 
+    /**
+     * Construtor para inicializar o objeto Conta a partir da agencia e numero
+     * @param agencia
+     * @param numero
+     */
     public Conta(int agencia, int numero){
         Conta.total++;
         //System.out.println("O total de contas é "+total);
@@ -59,6 +69,12 @@ public abstract class Conta {
     }
 
     public abstract void deposita(double valor);
+
+    /**
+     * Valor precisa ser maior do que o saldo
+     * @param valor
+     * @throws SaldoInsuficienteException
+     */
 
     public void saca(double valor) throws SaldoInsuficienteException{
         if(this.saldo < valor){
