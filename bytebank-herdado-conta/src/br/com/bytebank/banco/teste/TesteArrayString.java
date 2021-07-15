5d87756ae16ec4fbe9caa918bf1addc37f7382b1
Patrick Mota <patrickvieiramota@gmail.com>
2021-07-15 11:28:42 -0300

    Array string args

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayString.java
@@ -0,0 +1,13 @@
+package br.com.bytebank.banco.teste;
+
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
+
+public class TesteArrayString {
+    public static void main(String[] args) {
+        for(int i=0; i<args.length; i++){
+            System.out.println(args[i]);
+        }
+    }
+}
