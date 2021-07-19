87bba5d9fe6eedfa0c638193d9ea6dc2456a6849
Patrick Mota <patrickvieiramota@gmail.com>
2021-07-19 11:37:55 -0300

    Formas simplificadas de escrever um arquivo

---
+++ java-io/src/br/com/alura/java/io/teste/TesteEscritaPrintStream.java
@@ -0,0 +1,21 @@
+package br.com.alura.java.io.teste;
+
+import java.io.*;
+
+public class TesteEscritaPrintStream {
+    public static void main(String[] args) throws IOException {
+
+        //BufferedWriter bw = new BufferedWriter(new FileWriter("lorem2.txt"));
+
+        //PrintStream ps = new PrintStream("lorem2.txt");
+        //PrintStream ps = new PrintStream(new File("lorem2.txt"));
+
+        PrintWriter ps = new PrintWriter("lorem2.txt");
+
+        ps.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut");
+        ps.println();
+        ps.println("Testeando se esta funcionando!!!");
+
+        ps.close();
+    }
+}
