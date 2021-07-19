bcf468e11dc14d2c2afa0493002bd35c455dfd94
Patrick Mota <patrickvieiramota@gmail.com>
2021-07-19 14:28:03 -0300

    Adicionar charset na leitura e escrita dos arquivos

--- java-io/src/br/com/alura/java/io/teste/TesteEscritaPrintStreamPrintWriter.java
+++ java-io/src/br/com/alura/java/io/teste/TesteEscritaPrintStreamPrintWriter.java
@@ -10,7 +10,7 @@ public class TesteEscritaPrintStreamPrintWriter {
         //PrintStream ps = new PrintStream("lorem2.txt");
         //PrintStream ps = new PrintStream(new File("lorem2.txt"));
 
-        PrintWriter ps = new PrintWriter("lorem2.txt");
+        PrintWriter ps = new PrintWriter("lorem2.txt", "UTF-8");
 
         ps.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut");
         ps.println();
