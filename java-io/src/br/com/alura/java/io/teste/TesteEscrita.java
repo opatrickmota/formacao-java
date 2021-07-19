89ac9e65968718c7ac71cba04da06ecda2ae2d2a
Patrick Mota <patrickvieiramota@gmail.com>
2021-07-19 10:15:00 -0300

    Escrevendo arquivo com OutputStream e Writer

---
+++ java-io/src/br/com/alura/java/io/teste/TesteEscrita.java
@@ -0,0 +1,20 @@
+package br.com.alura.java.io.teste;
+
+import java.io.*;
+
+public class TesteEscrita {
+    public static void main(String[] args) throws IOException {
+
+        // Fluxo de saida com arquivo
+        OutputStream fos = new FileOutputStream("lorem2.txt");
+        Writer osw = new OutputStreamWriter(fos);
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        bw.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut");
+        bw.newLine();
+        bw.newLine();
+        bw.write("Testeando se esta funcionando!!!");        
+
+        bw.close();
+    }
+}
