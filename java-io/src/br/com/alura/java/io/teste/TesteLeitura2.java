1f5092f60443dff9b9a6bd9f34e76457b88ed851
Patrick Mota <patrickvieiramota@gmail.com>
2021-07-19 13:37:53 -0300

    Formatação de valores

--- java-io/src/br/com/alura/java/io/teste/TesteLeitura2.java
+++ java-io/src/br/com/alura/java/io/teste/TesteLeitura2.java
@@ -1,6 +1,8 @@
 package br.com.alura.java.io.teste;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteLeitura2 {
@@ -10,7 +12,23 @@ public class TesteLeitura2 {
 
         while (scanner.hasNextLine()){
             String linha = scanner.nextLine();
-            System.out.println(linha);
+
+            Scanner linhaScanner = new Scanner(linha);
+            linhaScanner.useLocale(Locale.US);
+            linhaScanner.useDelimiter(",");
+
+            String tipoConta = linhaScanner.next();
+            int agencia = linhaScanner.nextInt();
+            int numero = linhaScanner.nextInt();
+            String titular = linhaScanner.next();
+            double saldo = linhaScanner.nextDouble();
+
+            String valorFormatado = String.format(new Locale("pt", "BR"), "%s - %04d-%08d %s: %.3f %n", tipoConta, agencia, numero, titular, saldo);
+
+            System.out.println(valorFormatado);
+
+            //String[]valores = linha.split(",");
+            //System.out.println(Arrays.toString(valores));
         }
 
         scanner.close();
