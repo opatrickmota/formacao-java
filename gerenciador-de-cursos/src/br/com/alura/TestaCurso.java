8eb77db68ae5f421d7eb2b698d36e30b1afc0a55
Patrick Mota <patrickvieiramota@gmail.com>
2021-07-20 16:00:21 -0300

    Praticando Collections

--- gerenciador-de-cursos/src/br/com/alura/TestaCurso.java
+++ gerenciador-de-cursos/src/br/com/alura/TestaCurso.java
@@ -6,6 +6,7 @@ public class TestaCurso {
     public static void main(String[] args) {
         Curso javaColecoes = new Curso("Dominando as coleções do Java", "Paulo Silveira");
 
+        // aulas é imutavel pois usa programacao defensiva
         //javaColecoes.getAulas().add(new Aula("Trabalhando com ArrayList", 21)); // nao funciona mais pois agora e read-only
         javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
         javaColecoes.adiciona(new Aula("Criando uma aula", 20));
